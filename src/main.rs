35f980ff80c53a0dd0bff56cc47d7834b049cb86
hollow-owl <71401395+hollow-owl@users.noreply.github.com>
2022-05-29 19:55:22 -0500

    Stubs

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,62 @@
-use std::{collections::HashMap, ops::Sub};
+use std::{collections::HashMap, ops::Sub, hash::Hash};
 
+type Program = Vec<Rule>;
 struct Rule {
     head: Atom,
-    body: Vec<Atom>
+    body: Vec<Atom>,
 }
 
 #[derive(PartialEq, Eq)]
 struct Atom {
     pred_sym: String,
-    terms: Vec<Term>
+    terms: Vec<Term>,
 }
 
-#[derive(PartialEq, Eq,Clone,Hash)]
-enum Term {
+#[derive(PartialEq, Eq)]
+pub enum Term {
     Var(String),
-    Sym(String)
+    Sym(String),
 }
 
-type Program = Vec<Rule>;
 type KnowlegeBase = Vec<Atom>;
-type Substitution = HashMap<Term,Term>;
+type Substitution = HashMap<Term, Term>;
 
 fn empty_substition() -> Substitution {
     HashMap::new()
 }
 
-fn substitute(atom: Atom, sub: Substitution) -> Atom {
-    let terms = atom.terms.to_vec();
-    let terms = terms.iter().map(|t| sub.get(t).unwrap_or(t)).cloned().collect();
-    Atom {
-        terms,
-        ..atom
-    }
+fn solve(rules: Program) -> KnowlegeBase {
+    vec![]
+}
+
+fn is_range_restricted(r: Rule) -> Bool {
+    false
+}
+
+fn immediate_consequence(rules: Program, kb: KnowlegeBase) -> KnowlegeBase {
+    vec![]
+}
+
+fn eval_rule(kb: KnowlegeBase, Rule { head, body }: Rule) -> KnowlegeBase {
+    vec![]
+}
+
+fn walk(kb: KnowlegeBase) -> fn(Vec<Atom>) -> Vec<Substitution> {
+    |x| { vec![] }
+}
+
+fn eval_atom(kb: KnowlegeBase, atom: Atom, substitutions: Vec<Substitution>) -> Vec<Substitution> {
+    vec![]
+}
+
+fn substitute(atom: Atom, substitution: Substitution) -> Atom {
+    vec![]
 }
 
 fn unify(a: Atom, b: Atom) -> Option<Substitution> {
-    if a.pred_sym == b.pred_sym {
-        let terms = a.terms.into_iter().zip(b.terms.into_iter());
-        let mut subs = empty_substition();
-        for t in terms {
-            match t {
-                (sa @ Term::Sym(_), sb @ Term::Sym(_)) => if sa == sb {
-                    continue;
-                } else {
-                    return None;
-                }
-                (v @ Term::Var(_), s @ Term::Sym(_)) => {
-                    match subs.get(&v) {
-                        Some(ss) if &s != ss => return None,
-                        _ => {
-                            subs.insert(v, s);
-                        }
-                    }
-                }
-                (_, Term::Var(_)) => panic!("The second atom is assumed to be ground"),
-            }
-        }
-        Some(subs)
-    } else {
-        None
-    }
+    None
 }
 
 fn main() {
-    
     println!("Hello, world!");
 }
